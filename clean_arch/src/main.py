6aa1bd3bfa992af5032c0be501c20962282c3911
KizMan-23 <kizitougwu777@gmail.com>
2025-09-03 01:51:49 +0100

    beginnings for clean api architecture

---
+++ clean_arch/src/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from .database.core import engine, Base
+from .entities.todo import Todo
+from .entities.user import User
+
+
+app = FastAPI()
+
+# Base.metadata.create_all(bind=engine)
